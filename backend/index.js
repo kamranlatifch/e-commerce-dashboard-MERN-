0b312587c9e60aeed2ba36172635d24b0a8b2863
kamranlatifch <kamranlatif98@gmail.com>
2024-05-18 17:26:28 +0500

    e-comm(dbConnect)

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,14 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const app = express();
+const connectDB = async () => {
+  mongoose.connect("mongodb://localhost:27017/admin");
+  const productSchema = new mongoose.Schema({});
+  const productModel = mongoose.model("product", productSchema);
 
-app.get("/", (req, resp) => {
-  resp.send("app is working");
-});
+  const data = await productModel.find();
+
+  console.log("Data is", data);
+};
 
 app.listen(5000);
