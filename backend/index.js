bda46d92b69fba1b1e21ac124288c5a7634d7c27
kamranlatifch <kamranlatif98@gmail.com>
2024-05-18 20:29:35 +0500

    backend(login): add login api functionality

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,21 @@ app.use(cors());
 app.post("/register", async (req, resp) => {
   let user = new User(req.body);
   let result = await user.save();
+  result = result.toObject();
+  delete result.password;
   resp.send(result);
+});
+
+app.post("/login", async (req, resp) => {
+  if (req.body.email && req.body.password) {
+    let user = await User.findOne(req.body).select("-password");
+    if (user) {
+      resp.send(user);
+    } else {
+      resp.send({ result: "No User Found" });
+    }
+  } else {
+    resp.send({ result: "Please provide necessary credentials" });
+  }
 });
 app.listen(5000);
