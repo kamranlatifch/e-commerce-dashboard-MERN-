9f0dff2c21d3e0c18feb9a9e2ae7d6b34574cf2a
kamranlatifch <kamranlatif98@gmail.com>
2024-05-22 13:49:22 +0500

    fix(backend):fixed backend route issue

--- backend/index.js
+++ backend/index.js
@@ -69,12 +69,12 @@ app.get("/products", verifyTokenMiddleware, async (req, resp) => {
   }
 });
 
-app.get("product/:id", async (req, resp) => {
+app.get("/product/:id", async (req, resp) => {
   let result = await Product.findOne({ _id: req.params.id });
   if (result) {
     resp.send(result);
   } else {
-    resp.send({ result: "No record Found"});
+    resp.send({ result: "No record Found" });
   }
 });
 
