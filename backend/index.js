f5d8bdb282f8b2b875f5c2cfec84a7eae06b1a5b
Muhammad Kamran Latif <111248908+kamranlatifch@users.noreply.github.com>
2024-05-23 14:51:57 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.post("/register", async (req, resp) => {
   if (result) {
     Jwt.sign({ user }, jwtKey, { expiresIn: "2h" }, (err, token) => {
       if (err) {
-        resp.send({ result: "Something Went wrong" });
+        resp.send({ result: "Something Went Wrong" });
       } else {
         resp.send({ result, auth: token });
       }
