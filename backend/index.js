6391283432c72a3c0df44bb91bcd5be46768cc95
Muhammad Kamran Latif <111248908+kamranlatifch@users.noreply.github.com>
2024-06-20 22:17:08 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -60,7 +60,7 @@ app.post("/add-product", verifyTokenMiddleware, async (req, resp) => {
   resp.send(result);
 });
 
-// app.get("/products", verifyTokenMiddleware, async (req, resp) => {
+// app.get("/products", verifyTokenMiddleware, async (req, 
 
 
 
